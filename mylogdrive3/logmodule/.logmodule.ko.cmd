7052b9cf8b5a772f541c8f30772db51366f9a35c
dong <zhangdongpei@flyaudio.cn>
2018-06-29 19:20:08 +0800

    更新log.c\readlog.c\readlog.h

---
+++ mylogdrive3/logmodule/.logmodule.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/dong/code/mylogdrive2/logmodule/logmodule.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/dong/code/mylogdrive2/logmodule/logmodule.ko /home/dong/code/mylogdrive2/logmodule/logmodule.o /home/dong/code/mylogdrive2/logmodule/logmodule.mod.o ;  true
---
+++ mylogdrive3/logmodule/.tmp_versions/logmodule.mod
@@ -0,0 +1,3 @@
+/home/dong/code/mylogdrive2/logmodule/logmodule.ko
+/home/dong/code/mylogdrive2/logmodule/log.o
+
