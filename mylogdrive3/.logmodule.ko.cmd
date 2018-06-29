adc59d48ff459c4367e5e3f8d260639d99b52563
dong <zhangdongpei@flyaudio.cn>
2018-06-29 20:21:00 +0800

    update log.c,readlog.c and readlog.h

--- mylogdrive3/.logmodule.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/dong/code/mylogdrive/logmodule.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/dong/code/mylogdrive/logmodule.ko /home/dong/code/mylogdrive/logmodule.o /home/dong/code/mylogdrive/logmodule.mod.o ;  true
